88a782465df4d34117d298fbb73ae287c4c5b286
camillemoufle <camille.moufle@gmail.com>
2012-06-08 14:48:32 +0200

    Tentative de mise en parallèle de plusieurs activités

--- SoundGen-V1/src/pjl/appli/SoundGenActivity.java
+++ SoundGen-V1/src/pjl/appli/SoundGenActivity.java
@@ -1,28 +1,41 @@
 package pjl.appli;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
+import android.view.View.OnClickListener;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
-
 public class SoundGenActivity extends Activity {
     /** Called when the activity is first created. */
     
     public RadioGroup radioGroup ;
-
-	
-	
-    
+ 	public RadioButton fmButton;
+ 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     	radioGroup = (RadioGroup)findViewById(R.id.radioSyntheseSonore);
-    	
     }
+    
+   /* OnClickListener granularButtonConnexion = new OnClickListener()
+    {
+     @Override
+     public void onClick(View actuelView){
+     // On met en place le passage entre les deux activités sur ce Listener
+     Intent intent = new Intent(MainViewActivity.this,DisplayLoginActivity.class);
+     startActivity(intent);
+     }
+    };
+     
+    //On récupere le bouton souhaité et on lui affecte le Listener
+    RadioButton granularButton = (RadioButton) findViewById(R.layout.main);
+    granularButton.setOnClickListener(granularButtonConnexion);*/
+    
+    
     
     
     
@@ -32,22 +45,15 @@ public class SoundGenActivity extends Activity {
 		coucou = new TextView(this);
 		coucou.setText("Bonjour, vous etes en mode FM");
 		setContentView(coucou);
-    	
     }
     
     public void onGranulaireButtonClicked(View v) {
         // Ce qui se produit lorsqu'on clique sur la synthèse Granulaire
-    	RadioButton rb = (RadioButton) v;
-        Toast.makeText(this, rb.getText(), Toast.LENGTH_SHORT).show();
-    	
-        
+        //Toast.makeText(this, rb.getText(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(SoundGenActivity.this, ModeGranulaireActivity.class);
+        startActivity(intent);
     } 
     
-    public boolean onTouchEvent( MotionEvent event) {
-    	Toast.makeText(this, ""+event.getX()+","+event.getY(), Toast.LENGTH_SHORT).show();
-		
-    	
-		return false;
-	}
+   
 
 }
