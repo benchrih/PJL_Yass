e603b82a00b0944cb3b87edccb7ccf54cb0d2335
camillemoufle <camille.moufle@gmail.com>
2012-06-08 09:31:17 +0200

    Version avec affichage des coordonnées des points touchés. Il faut maintenant qu'on essaye de mettre la fonction onTouchView dans la classe SynthGranulaire qui pour le moment ne sert à rien.

--- SoundGen-V1/src/pjl/appli/SoundGenActivity.java
+++ SoundGen-V1/src/pjl/appli/SoundGenActivity.java
@@ -1,28 +1,19 @@
 package pjl.appli;
 
 import android.app.Activity;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.*;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//hshgdhsd
 public class SoundGenActivity extends Activity {
     /** Called when the activity is first created. */
     
-    private RadioGroup radioGroup ;
-    private SynthGranulaire synthGranulaire = new SynthGranulaire();
+    public RadioGroup radioGroup ;
 
-	public Activity activity;
-    public SoundGenActivity (Activity act){
-		this.activity=act;
-	}
+	
 	
     
     @Override
@@ -42,19 +33,21 @@ public class SoundGenActivity extends Activity {
 		coucou.setText("Bonjour, vous etes en mode FM");
 		setContentView(coucou);
     	
-    	//RadioButton rb = (RadioButton) v;
-        //Toast.makeText(this, rb.getText(), Toast.LENGTH_SHORT).show();
     }
     
     public void onGranulaireButtonClicked(View v) {
         // Ce qui se produit lorsqu'on clique sur la synthèse Granulaire
     	RadioButton rb = (RadioButton) v;
         Toast.makeText(this, rb.getText(), Toast.LENGTH_SHORT).show();
-    	//SynthGranulaire synthGranulaire = new SynthGranulaire();
-    	//MotionEvent event = null;
-    	//synthGranulaire.onTouch(v, event);
+    	
         
     } 
     
+    public boolean onTouchEvent( MotionEvent event) {
+    	Toast.makeText(this, ""+event.getX()+","+event.getY(), Toast.LENGTH_SHORT).show();
+		
+    	
+		return false;
+	}
 
 }
