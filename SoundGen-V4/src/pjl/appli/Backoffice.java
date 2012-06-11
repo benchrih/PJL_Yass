632e4fed97ffaf9121f25c6705ea8f6398b707ae
benchrih <benchrih.yassine@gmail.com>
2012-06-11 18:18:58 +0200

    dernier commit de la V4

---
+++ SoundGen-V4/src/pjl/appli/Backoffice.java
@@ -0,0 +1,17 @@
+package pjl.appli;
+
+import android.os.AsyncTask;
+
+class Backoffice extends AsyncTask<Void, Void, Void>{
+ 
+
+	@Override
+	protected Void doInBackground(Void... params) {
+		
+		
+
+		
+		return null;
+	}
+
+}
